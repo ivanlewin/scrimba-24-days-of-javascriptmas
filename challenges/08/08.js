170da967e80b386bc4b83dc7f1a692f6919cb1db
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2021-04-04 02:16:08 -0300

    challenge 08 completed

--- challenges/08/08.js
+++ challenges/08/08.js
@@ -11,6 +11,31 @@ event listeners, Math.random()
 
 // Write your code here 👇
 
+const dice = document.querySelector(".dice");
+const dots = Array.from(document.querySelectorAll(".dot"));
+const diceRoll = document.getElementById("dice-roll")
+
+const numbers = {
+	"1": "one",
+	"2": "two",
+	"3": "three",
+	"4": "four",
+	"5": "five",
+	"6": "six",
+}
+
+dice.addEventListener("click", () => {
+	const rolled = Math.floor(Math.random() * 6) + 1;
+	const rolledClass = numbers[rolled].toString();
+
+	dots.forEach(dot => dot.classList.add("white")); // clear all dots
+
+	// remove the white (hidden) class from the dots needed to make the dice face
+	const rolledDots = dots.filter(dot => dot.classList.contains(rolledClass));
+	rolledDots.forEach(dot => dot.classList.remove("white"));
+
+	diceRoll.textContent = rolled;
+})
 
 /*
 
